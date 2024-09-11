d02a5b3acfc24f02ece5c1440cba8511728e082d
gogomos <abououss2000@gmail.com>
2024-09-11 16:31:40 +0100

    handle the activateUser

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -53,11 +53,6 @@ export const registrationUser = CatchAsyncError(
       } catch (error: any) {
         return next(new ErrorHandler(error.message, 400));
       }
-
-    //   res.status(201).json({
-    //     success: true,
-    //     user,
-    //   });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 400));
     }
@@ -83,3 +78,38 @@ export const createActivationToken = (user: any): IActivationToken => {
     token,
   };
 };
+
+//activate user
+ interface IActivationRequest {
+  activation_token: string;
+  activation_code: string;
+ }
+
+ export const activateUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { activation_token, activation_code } = req.body as IActivationRequest;
+     const newUser: {user: IUser, activationCode: string} = jwt.verify(
+      activation_token,
+      process.env.ACTIVATION_TOKEN_SECRET as Secret
+    ) as {user: IUser, activationCode: string};
+    if (newUser.activationCode !== activation_code) {
+      return next(new ErrorHandler("Invalid activation code", 400));
+    }
+
+    const { name, email, password } = newUser.user;
+    const existUser = await userModel.findOne({ email });
+    if (existUser) {
+      return next(new ErrorHandler("User already exists", 400));
+    }
+    const user = await userModel.create({ name, email, password });
+    res.status(201).json({
+      success: true,
+      message: "Account has been activated",
+      user,
+    });
+  } catch (error: any) {
+    return next(new ErrorHandler(error.message, 400));
+  }
+})
+
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -2,10 +2,12 @@
 
 import express from "express";
 // import { registrationUser } from "../controllers/user.controller";
-import {registrationUser} from '../controllers/user.controller'
+import {registrationUser} from '../controllers/user.controller';
+import {activateUser} from '../controllers/user.controller';
 const userRouter = express.Router();
 
 
 userRouter.post('/registration', registrationUser);
+userRouter.post('/activate-user', activateUser);
 
 export default userRouter;
