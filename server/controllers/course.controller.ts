56dd8822795f355eb2c6b0f0adf3839bbdc55eb2
gogomos <abououss2000@gmail.com>
2024-09-23 04:38:15 +0100

    add courses

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -4,7 +4,9 @@ import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import ErrorHandler from "../utils/ErrorHandler";
 import CourseModel, { ICourse } from "./../models/course.model";
 import cloudinary from "cloudinary";
+import { redis } from "../utils/redis";
 import { createCourse } from "../services/course.service";
+import mongoose from "mongoose";
 require("dotenv").config();
 
 export const uploadCourse = CatchAsyncError(
@@ -68,15 +70,168 @@ export const editCourse = CatchAsyncError(
 export const getSingleCourse = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const course = await CourseModel.findById(req.params.id).select(
-        "-courseData.videoUrl -courseData.suggestion -courseData.question -courseData.links"
+      const courseId = req.params.id;
+      const isCachExist = await redis.get(courseId);
+      if (isCachExist) {
+        const course = JSON.parse(isCachExist);
+        res.status(200).json({
+          success: true,
+          course,
+        });
+      } else {
+        const course = await CourseModel.findById(req.params.id).select(
+          "-coursesData.videoUrl -coursesData.suggestion -coursesData.questions -coursesData.links"
+        );
+        await redis.set(courseId, JSON.stringify(course));
+        res.status(200).json({
+          succsses: true,
+          course,
+        });
+      }
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+//get all course
+
+export const getAllCourses = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const isCachExist = await redis.get("allCourses");
+      if (isCachExist) {
+        const courses = JSON.parse(isCachExist);
+        res.status(200).json({
+          success: true,
+          courses,
+        });
+      } else {
+        const courses = await CourseModel.find().select(
+          "-coursesData.videoUrl -coursesData.suggestion -coursesData.questions -coursesData.links"
+        );
+        await redis.set("allCourses", JSON.stringify(courses));
+        res.status(200).json({
+          success: true,
+          courses,
+        });
+      }
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+// get course content // only for valid user
+export const getCourseByUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userCourseList = req.user?.courses;
+      const courseId = req.params.id;
+      const courseExists = userCourseList.find(
+        (course: any) => course._id.toString() === courseId
       );
+      if (!courseExists) {
+        return next(
+          new ErrorHandler("You are not enrolled in this course", 404)
+        );
+      }
+      const course = await CourseModel.findById(courseId);
+      const content = course?.coursesData;
       res.status(200).json({
-        succsses: true,
-        course,
+        success: true,
+        content,
       });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 500));
     }
   }
 );
+
+// add question in course
+
+interface IAddQuestionData {
+  question: string;
+  courseId: string;
+  contentId: string;
+}
+
+export const addQuestion = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { question, courseId, contentId } : IAddQuestionData = req.body;
+
+      const course = await CourseModel.findById(courseId);
+      if (!mongoose.Types.ObjectId.isValid(contentId)) {
+        return next(new ErrorHandler("Invalid content id", 400));
+      }
+      const courseContent = course?.coursesData?.find(
+        (content: any) => content._id.toString() === contentId
+      )
+      if (!courseContent) {
+        return next(new ErrorHandler("Course content not found", 404));
+      }
+      const questionData: any = {
+        question,
+        user: req.user,
+        questionReplies: [],
+      };
+
+      courseContent.questions.push(questionData);
+      await course?.save();
+      res.status(200).json({
+        success: true,
+        message: "Question added successfully",
+        course,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  });
+
+  // add answer in course question
+
+interface IAnswerData {
+  answer: string;
+  questionId: string;
+  courseId: string;
+  contentId: string;
+}
+
+export const addAnswer = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { answer, questionId, courseId, contentId } : IAnswerData = req.body;
+
+      const course = await CourseModel.findById(courseId);
+      if (!mongoose.Types.ObjectId.isValid(contentId)) {
+        return next(new ErrorHandler("Invalid content id", 400));
+      }
+      const courseContent = course?.coursesData?.find(
+        (content: any) => content._id.toString() === contentId
+      )
+      if (!courseContent) {
+        return next(new ErrorHandler("Course content not found", 404));
+      }
+      const question = courseContent.questions.find(
+        (question: any) => question._id.toString() === questionId
+      );
+      if (!question) {
+        return next(new ErrorHandler("Question not found", 404));
+      }
+      const answerData: any = {
+        answer,
+        user: req.user,
+      };
+
+      question.questionReplies.push(answerData);
+      await course?.save();
+      res.status(200).json({
+        success: true,
+        message: "Answer added successfully",
+        course,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  });
